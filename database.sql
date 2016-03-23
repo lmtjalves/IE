2ee13d05d7880336718f6b71c64d633f1fc616f0
root <root@soabpm-vm.site>
2016-03-23 10:36:57 -0700

    Implemented CreateClientService

--- database.sql
+++ database.sql
@@ -1,14 +1,15 @@
 # Stores client accounts
+CREATE sequence seq_clientId start with 1 increment by 1;
+
 CREATE TABLE client (
-	id NUMERIC(10),
+	id NUMBER(10),
 	email VARCHAR(255),
 	password VARCHAR(255),
 	district VARCHAR(255),
 	county VARCHAR(255),
 	street VARCHAR(255),
-	door_number VARCHAR(20),
+	door_number NUMERIC(5),
 	zip_code VARCHAR(255),
 	phone_number VARCHAR(20),
-
 	PRIMARY KEY(id)
 );
