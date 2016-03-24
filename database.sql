b07a1c7b72b2f5ed4d4e912d8241a56575e1878e
root <root@soabpm-vm.site>
2016-03-24 08:58:55 -0700

    Implemented ClientExists service

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
-# Stores client accounts
+# Sequence for client id
 CREATE sequence seq_clientId start with 1 increment by 1;
 
+# Stores clients information
 CREATE TABLE client (
 	id NUMBER(10),
 	email VARCHAR(255),
@@ -8,7 +9,7 @@ CREATE TABLE client (
 	district VARCHAR(255),
 	county VARCHAR(255),
 	street VARCHAR(255),
-	door_number NUMERIC(5),
+	door_number VARCHAR(255),
 	zip_code VARCHAR(255),
 	phone_number VARCHAR(20),
 	PRIMARY KEY(id)
