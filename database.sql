ae38e3850d19fa101079a892e9aef37d34cc2bdd
Oracle <lmtjalves@gmail.com>
2016-04-21 13:52:55 -0700

    CalculatePayment service reviewed

--- database.sql
+++ database.sql
@@ -31,12 +31,13 @@ CREATE TABLE transporter (
 CREATE sequence seq_transportationId start with 1 increment by 1;
 
 # Stores all transportations
+# Paid may be N - Not paid, P - Paid, W - waiting (some process is managing the payment
 CREATE TABLE transportation (
 	id NUMBER(10),
 	transporterId NUMBER(10) NOT NULL,
 	clientId NUMBER(10) NOT NULL,
 	quotation NUMBER(10) NOT NULL,
-	paid NUMBER(1) DEFAULT 0 NOT NULL,
+	paid VARCHAR(1) DEFAULT 'N' NOT NULL,
 	createdAt 
 	PRIMARY KEY(id),
 	CONSTRAINT fk_transporter 
@@ -50,4 +51,14 @@ CREATE TABLE transportation (
 INSERT INTO transporter (id, name) VALUES (seq_transporterId.nextval, 'TransporterA');
 INSERT INTO transporter (id, name) VALUES (seq_transporterId.nextval, 'TransporterX');
 
-
+CREATE OR REPLACE PROCEDURE missingPaymentsFor(transporter_id IN NUMERIC, total OUT NUMERIC, first_id OUT NUMERIC, last_id OUT NUMERIC)
+IS
+BEGIN
+	SELECT SUM(quotation), MAX(id), MIN(id) 
+	INTO total, first_id, last_id
+	FROM transportation
+	WHERE paid='N' AND transporterId=transporter_id;
+	UPDATE transportation SET paid='W' WHERE paid='N' AND transporterId=transporter_id;
+	COMMIT;
+END missingPaymentsFor;
+/	
